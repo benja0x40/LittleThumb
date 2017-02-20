576f90345a7b395b83dd84d3f19ac73e5dd6ae0d
benja0x40 <benjaminolivierleblanc@gmail.com>
2017-02-20 15:51:52 +0100

    Initial commit

---
+++ R/BaseSpace.R
@@ -0,0 +1,65 @@
+# =============================================================================.
+# STUPID: a shell session started and terminated for each call of system()
+# -----------------------------------------------------------------------------.
+# basespace_mount <- function(mnt) {
+#   if(! file.exists(mnt)) {
+#     if(! execute(paste0('mkdir -p "', mnt, '"'))) {
+#       stop("unusable mount path ", mnt)
+#     }
+#   }
+#   if(! execute(paste0('basemount "', mnt, '"'))) {
+#     stop("basespace mount command failed")
+#   }
+#   T
+# }
+# =============================================================================.
+# STUPID: a shell session started and terminated for each call of system()
+# -----------------------------------------------------------------------------.
+# basespace_unmount <- function(mnt) {
+#   if(! file.exists(mnt)) {
+#     stop("non-existent mount path ", mnt)
+#   }
+#   if(! execute(paste0('basemount --unmount "', mnt, '"'))) {
+#     stop("basespace unmount command failed")
+#   }
+#   T
+# }
+# =============================================================================.
+#
+# -----------------------------------------------------------------------------.
+basespace_available <- function(mnt) {
+  file.exists(paste0(mnt, "/Projects"))
+}
+# =============================================================================.
+#
+# -----------------------------------------------------------------------------.
+basespace_projects <- function(mnt) {
+  dir(paste0(mnt, "/Projects"))
+}
+# =============================================================================.
+#
+# -----------------------------------------------------------------------------.
+basespace_properties <- function(x) {
+  rex <- "^([^:]+): (.*)$"
+  x <- readLines(x)
+  k <- gsub(rex, "\\1", x)
+  v <- gsub(rex, "\\2", x)
+  names(v) <- make.names(k)
+  v
+}
+# =============================================================================.
+#
+# -----------------------------------------------------------------------------.
+basespace_dataset <- function(mnt, prj) {
+  fpath <- paste0(mnt, "/Projects/", prj, "/Samples")
+  flist <- paste0(fpath, "/", dir(fpath), "/SampleProperties")
+  ann <- c()
+  for(fname in flist) {
+    ann <- rbind(ann, basespace_properties(fname))
+  }
+  ann <- data.frame(ann, stringsAsFactors = F)
+  ann$project        <- prj
+  ann$basemount.host <- host_name()
+  ann$basemount.path <- paste0(dirname(flist), "/Files")
+  ann
+}
---
+++ R/MappingOperations.R
@@ -0,0 +1,77 @@
+# =============================================================================.
+#
+# -----------------------------------------------------------------------------.
+availableMappingIndexes <- function(midx) {
+
+  host <- Sys.info()["nodename"]
+  midx <- midx[midx$hostname == host,]
+
+  txt <- midx$sequence_path
+  chk <- file.exists(txt)
+  midx$sequence_available <- chk
+
+  txt <- paste0(midx$sequence_path, ".gz")
+  tst <- file.exists(txt)
+  midx$sequence_path[tst & ! chk] <- txt[tst & ! chk]
+  chk <- chk | tst
+  midx$sequence_available <- chk
+
+  midx$index_available <- file.exists(dirname(midx$index_path))
+  midx
+}
+# =============================================================================.
+#
+# -----------------------------------------------------------------------------.
+suggestMappingIndexes <- function(ann, midx, cmd) {
+
+  ann$mapping_index <- ""
+  x <- ann$organism_ch1
+  for(i in 1:length(x)) {
+    r <- genomeBuilds(x[i], style = "UCSC")
+    for(j in nrow(r):1) {
+      asm <- as.character(r[j, 3])
+      chk <- with(midx, ucscID == asm & mapper == cmd)
+      if(sum(chk) > 1) warning("multiple mapping indexes available")
+      if(sum(chk) > 0) {
+        ann$mapping_index[i] <- midx$identifier[which(chk)[1]]
+        break
+      }
+    }
+  }
+  ann
+}
+# =============================================================================.
+#
+# -----------------------------------------------------------------------------.
+validateMappingCommands <- function(x, mappers) {
+
+  # Test if mapper is installed
+  chk <- sapply(paste0("type ", x, "&>/dev/null"), system, intern = T)
+  chk <- grepl(" not found$", chk, perl = T)
+  if(any(chk)) {
+    stop("mapper not installed ", paste(x[chk], collapse = ", "), call. = F)
+  }
+
+  # Test if mapper is supported
+  chk <- x %in% mappers$command
+  if(! all(chk)) {
+    stop("mapper not supported ", paste(x[! chk], collapse = ", "), call. = F)
+  }
+}
+# =============================================================================.
+#
+# -----------------------------------------------------------------------------.
+validateMappingIndexes <- function(x, midx, cmd) {
+
+  # Test if identifier is defined
+  chk <- x %in% midx$identifier
+  if(! all(chk)){
+    stop("undefined index ", paste(x[! chk], collapse = ", "), call. = F)
+  }
+
+  # Test if mapping index is available
+  chk <- filter(midx, mapper == cmd & identifier %in% x)$index_available
+  if(! all(chk)) {
+    stop("index not available ", paste(x[! chk], collapse = ", "), call. = F)
+  }
+}
